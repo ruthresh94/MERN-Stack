a09046f3fa441a81ad58db04ca4182371a8d8cfc
ruthresh94 <ruthresh.t2022ece@sece.ac.in>
2024-02-29 15:10:31 +0530

    Day4

---
+++ day4.js
@@ -0,0 +1,32 @@
+/*let a=5;
+for(console.log("a");a>0;console.log("b")){
+    console.log("c");
+    a--;
+}let a=100;
+if(a%3)
+console.log("Hi");
+if(a%5)
+console.log("Hello");
+let a=0;
+for(a;a<100;a++);
+    console.log(a);*/
+//Normal Functions
+//Non-anonymous
+function func(a){
+    return "Hello" + ' '+ a;
+}
+console.log(func("RUTHRESH"));
+//Anonymous
+let b=function(b)
+{
+    return "Hello" + ' '+ b;
+}
+console.log(b("Waseef"));
+
+//Arrow Function
+let c=(c)=>{
+    return "Hello" + ' '+ c;
+}
+console.log(c("Velmurugan"));
+
+
