b157f34a1a777f46e69904e6fcd09310271b7572
ruthresh94 <ruthresh.t2022ece@sece.ac.in>
2024-03-02 22:16:06 +0530

    Login

---
+++ Login/script.js
@@ -0,0 +1,17 @@
+const togglePassword = document.getElementById('showpassword');
+const passwordInput = document.getElementById('password');
+
+togglePassword.addEventListener('click', function() {
+    const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordInput.setAttribute('type', type);
+    this.classList.toggle('fa-eye-slash');
+});
+
+const toggleConfirmPassword = document.getElementById('showConfirmPassword');
+const confirmPasswordInput = document.getElementById('confirmPassword');
+
+toggleConfirmPassword.addEventListener('click', function() {
+    const type = confirmPasswordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+    confirmPasswordInput.setAttribute('type', type);
+    this.classList.toggle('fa-eye-slash');
+});
